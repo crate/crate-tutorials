8587907b0d430707847c77dec939b6f357b547d8
Andreas Motl <andreas.motl@crate.io>
2021-03-30 12:36:35 +0200

    Improve linking to other parts of the documentation using intersphinx
    In the long run, this should happen in crate-docs-theme already.
    In order to provide headroom for this, the addition here is made in a
    graceful manner.

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,17 @@
 from crate.theme.rtd.conf.crate_tutorials import *
+
+
+if "sphinx.ext.intersphinx" not in extensions:
+    extensions += ["sphinx.ext.intersphinx"]
+
+
+if "intersphinx_mapping" not in globals():
+    intersphinx_mapping = {}
+
+
+intersphinx_mapping.update({
+    'reference': ('https://crate.io/docs/crate/reference/', None),
+    'howtos': ('https://crate.io/docs/crate/howtos/', None),
+    'admin-ui': ('https://crate.io/docs/crate/admin-ui/', None),
+    'crash': ('https://crate.io/docs/crate/crash/', None),
+    })
